61c7d0397e1fd33f32bc260d4a33461eab19fc3d
MarkusHeine <markusheine.dev@gmail.com>
2020-02-05 11:05:29 +0100

    delete images

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import * as bodyParser from "body-parser";
+import logger from "morgan";
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 8080;
 
 const indexRouter = require("./routes/index");
 const userAPIRouter = require("./routes/userAPI");
 
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(logger("dev"));
 
 app.use("/index", indexRouter);
 app.use("/userAPI", userAPIRouter);
